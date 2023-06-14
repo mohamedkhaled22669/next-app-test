1ee06377295a58738375a3249ae0f0433e32a44e
mohamedkhaled22669 <51810133+mohamedkhaled22669@users.noreply.github.com>
2023-06-14 22:48:54 +0300

    v4

--- pages/api/posts/[id].js
+++ pages/api/posts/[id].js
@@ -1,11 +1,11 @@
-import {posts} from '../../../posts'
+// import {posts} from '../../../posts'
 
 export default function handlerApi(req,res) {
     let id = req.query.id;
 
-    let post = posts.find(p => p.id == id)
-    if(post) {
-        res.status(200).json(post)
+    // let post = posts.find(p => p.id == id)
+    if(true) {
+        res.status(200).json({email : "emailllll"})
     }else {
         res.status(400).json({err:"Ther is no post"})
     }
