6128898415c978807d2585bb4e542a3dc726add2
mohamedkhaled22669 <51810133+mohamedkhaled22669@users.noreply.github.com>
2023-06-14 22:59:01 +0300

    v6

--- pages/posts/[id].js
+++ pages/posts/[id].js
@@ -3,39 +3,40 @@ import React from 'react'
 export default function Detalis(props) {
     return (
         <div >
-            {props.post.title}
-            {props.post.body}
+            {/* {props.post.title}
+            {props.post.body} */}
+            test
         </div>
     )
 }
 
 
-export async function getStaticPaths () {
-    const res = await fetch('http://localhost:3000/api/posts')
-    const data = await res.json()
+// export async function getStaticPaths () {
+//     const res = await fetch('http://localhost:3000/api/posts')
+//     const data = await res.json()
 
-    const paths = data.map(d => {
-        return {
-            params: {id : `${d.id}`}
-        }
-    })
+//     const paths = data.map(d => {
+//         return {
+//             params: {id : `${d.id}`}
+//         }
+//     })
 
-    return {
-        paths: paths,
-        fallback:false
-    }
-}
+//     return {
+//         paths: paths,
+//         fallback:false
+//     }
+// }
 
-export async function getStaticProps(context) {
-    const res = await fetch(`http://localhost:3000/api/posts/${context.params.id}`)
-    const data = await res.json()
+// export async function getStaticProps(context) {
+//     const res = await fetch(`http://localhost:3000/api/posts/${context.params.id}`)
+//     const data = await res.json()
 
-    return{
-        props: {
-            post:data
-        }
-    }
-}
+//     return{
+//         props: {
+//             post:data
+//         }
+//     }
+// }
 
 // if i want requist all time want this post use alon
 // export async function getServerSideProps(context) {
