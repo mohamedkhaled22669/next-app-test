f024b05ff90a60055d853a5fbd5dc75323a21ab4
mohamedkhaled22669 <51810133+mohamedkhaled22669@users.noreply.github.com>
2023-06-14 23:01:14 +0300

    v6

--- pages/posts/index.js
+++ pages/posts/index.js
@@ -4,25 +4,26 @@ import React from 'react'
 export default function Posts(props) {
     return (
         <div>
-            {props.posts.map(post => (
+            {/* {props.posts.map(post => (
                 <div key={post.id}>
                     <Link href={`/posts/${post.id}`} >
                         {post.title}
                     </Link>
                 </div>
 
-            ))}
+            ))} */}
+            a7ms
         </div>
     )
 }
 
-export async function getStaticProps() {
-    const res = await fetch('http://localhost:3000/api/posts')
-    const data = await res.json()
+// export async function getStaticProps() {
+//     const res = await fetch('http://localhost:3000/api/posts')
+//     const data = await res.json()
 
-    return {
-        props: {
-            posts: data
-        }
-    }
-}
+//     return {
+//         props: {
+//             posts: data
+//         }
+//     }
+// }
