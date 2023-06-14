b4f797a5efefbaf501d404ff2b0ad3fed4cc9643
mohamedkhaled22669 <51810133+mohamedkhaled22669@users.noreply.github.com>
2023-06-14 23:22:34 +0300

    v7

--- pages/posts/[id].js
+++ pages/posts/[id].js
@@ -3,40 +3,39 @@ import React from 'react'
 export default function Detalis(props) {
     return (
         <div >
-            {/* {props.post.title}
-            {props.post.body} */}
-            test
+            {props.post.title}
+            {props.post.body}
         </div>
     )
 }
 
 
-// export async function getStaticPaths () {
-//     const res = await fetch('http://localhost:3000/api/posts')
-//     const data = await res.json()
+export async function getStaticPaths () {
+    const res = await fetch('https://jsonplaceholder.typicode.com/posts')
+    const data = await res.json()
 
-//     const paths = data.map(d => {
-//         return {
-//             params: {id : `${d.id}`}
-//         }
-//     })
+    const paths = data.map(d => {
+        return {
+            params: {id : `${d.id}`}
+        }
+    })
 
-//     return {
-//         paths: paths,
-//         fallback:false
-//     }
-// }
+    return {
+        paths: paths,
+        fallback:false
+    }
+}
 
-// export async function getStaticProps(context) {
-//     const res = await fetch(`http://localhost:3000/api/posts/${context.params.id}`)
-//     const data = await res.json()
+export async function getStaticProps(context) {
+    const res = await fetch(`https://jsonplaceholder.typicode.com/posts/${context.params.id}`)
+    const data = await res.json()
 
-//     return{
-//         props: {
-//             post:data
-//         }
-//     }
-// }
+    return{
+        props: {
+            post:data
+        }
+    }
+}
 
 // if i want requist all time want this post use alon
 // export async function getServerSideProps(context) {
